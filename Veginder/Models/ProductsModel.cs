4b41b45520bc98dc95899404a890fb66a3595471
Вера Пирог <vera.planet3@gmail.com>
2021-03-24 02:09:58 +0200

    new mvc for products, links between controllers

---
+++ Veginder/Models/ProductsModel.cs
@@ -0,0 +1,18 @@
+﻿using BLL.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Veginder.Models
+{
+	public class ProductsModel
+	{
+		public List<Product> Products;
+
+		public ProductsModel(List<Product> products)
+		{
+			Products = products;
+		}
+	}
+}
